6ce5f3f100574d40237d49b5a223fe80299ea075
ylwu <ylwu@mit.edu>
2014-10-20 20:27:24 -0400

    add login

--- testscripts/test.js
+++ testscripts/test.js
@@ -1,4 +1,12 @@
 //host login
+var roomNumber;
 
+$.post( "localhost:3000/create", 
+        { email: "host@gmail.com", 
+          name: "host" 
+          passowrd: "password" })
+        .done(function(data){
+            roomNumber = $.parseJSON(data);
+        });
 
 //API testing methods
