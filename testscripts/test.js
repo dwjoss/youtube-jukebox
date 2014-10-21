b40b2033d76cac34df38ce836d3538b9d149601e
Dylan Joss <djoss@mit.edu>
2014-10-20 20:28:04 -0400

    start implementing tests

--- testscripts/test.js
+++ testscripts/test.js
@@ -1,4 +1,30 @@
-//host login
+// Host login
 
 
-//API testing methods
+// Testing API Methods
+
+$.ajax({
+    type: 'POST',
+    dataType: 'jsonp',
+    url: 'http://localhost:3000/api/search',
+    data: { 
+        'query': 'MIT', 
+    },
+    success: function(msg){
+        console.log('Tested api/search');
+        console.log('YouTube search results: ' + msg);
+    }
+});
+
+$.ajax({
+    type: 'POST',
+    dataType: 'jsonp',
+    url: 'http://localhost:3000/api/join',
+    data: {
+        'roomNumber': 123
+    },
+    success: function(msg) {
+        console.log('Tested api/join');
+        console.log('Logged in users: ' + msg);
+    }
+});
