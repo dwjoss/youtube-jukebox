5c50f531b5e947ed90317a46c28128cbac128561
Kulpreet Chilana <contact@kulpreet.com>
2014-10-24 01:05:58 -0400

    Added in template for login page. Also set up views for host and participant.

---
+++ public/js/login.js
@@ -0,0 +1,41 @@
+$(function() {
+	// Handle Submission of the Login Form using AJAX
+	$("#login-form").submit(function(e)
+	{
+		$('#alert-error').hide(); 
+	    $.ajax(
+	    {
+	        url : $(this).attr("action"),
+	        type: "POST",
+	        data : $(this).serializeArray(),
+			success: function(data, textStatus) {
+				window.location.href = '/';
+			},
+	        error: function(data, textStatus, errorThrown) 
+	        {
+	            $('#alert-error').show();     
+	        }
+	    });
+	    e.preventDefault();
+	});
+	
+	// Handle Submission of the Create Accounts form using AJAX
+	$("#create-account-form").submit(function(e)
+	{
+		$('#create-error').hide();
+	    $.ajax(
+	    {
+	        url : $(this).attr("action"),
+	        type: "POST",
+	        data : $(this).serializeArray(),
+			success: function(data, textStatus) {
+				window.location.href = '/';
+			},
+	        error: function(jqXHR, textStatus, errorThrown) 
+	        {
+	            $('#create-error').show();     
+	        }
+	    });
+	    e.preventDefault();
+	});
+});
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,11 @@ router.get('/', function(req, res) {
 });
 
 router.get(new RegExp('room\/(.+)'), function(req, res) {
-    res.render('index');
+	if (req.user && (req.user.room.toString() === req.params[0])) {
+		res.render('host');
+	} else {
+		res.render('participant');
+	}
 });
 
 router.get('/login', function(req, res) {
