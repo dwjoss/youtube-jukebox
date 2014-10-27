159bf846d205ab4354ce1c1ea5efcb93daf23c47
Dylan Joss <djoss@mit.edu>
2014-10-26 22:40:07 -0400

    work on handlebar-ing the search results

---
+++ utils/utils.js
@@ -0,0 +1,23 @@
+var utils = {}
+
+/*
+    Convert a string of a number of seconds into the format
+    mm:ss
+*/
+utils.parseTime = function(secs) {
+    var secs = parseInt(secs);
+    
+    var mm = Math.floor(secs/60).toString();
+    var ss = (secs % 60).toString();
+
+    if (mm.length === 1) {
+        mm = '0' + mm;
+    }
+    if (ss.length === 1) {
+        ss = '0' + ss;
+    }
+
+    return mm + ':' + ss;
+}
+
+module.exports = utils;
